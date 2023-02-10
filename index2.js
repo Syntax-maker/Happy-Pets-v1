1da2ba5bfdceb7511b7e3b5ce1f00744120dda92
Syntax-maker <124785914+Syntax-maker@users.noreply.github.com>
2023-02-10 08:10:52 +0800

    Update index2.js
    I added Isotope Js for the button filter

--- index2.js
+++ index2.js
@@ -1,3 +1,24 @@
+// init Isotope
+var $grid = $('.collection-list').isotope({
+    // options
+  });
+  // filter items on button click
+  $('.filter-button-group').on( 'click', 'button', function() {
+    var filterValue = $(this).attr('data-filter');
+    resetFilterBtns();
+    $(this).addClass('active-filter-btn');
+    $grid.isotope({ filter: filterValue });
+  });
+  
+  var filterBtns = $('.filter-button-group').find('button');
+  function resetFilterBtns(){
+    filterBtns.each(function(){
+      $(this).removeClass('active-filter-btn');
+    });
+  }
+
+
+
 
 // get all elements with class name 'btns'
 const btn = document.getElementsByClassName('btns')
